ea4916d2ff1ec8114613f3ce47e9a36e3c452606
osama <osamaabukamel.12345@gmail.com>
2023-07-25 12:32:22 +0300

    Create LoopsII.java

---
+++ HackerRank/LoopsII.java
@@ -0,0 +1,22 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class LoopsII {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int q = scanner.nextInt();
+        for (int i = 0; i < q; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int n = scanner.nextInt();
+            int sum = a;
+            for (int j = 0; j < n; j++) {
+                sum += Math.pow(2, j) * b;
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
