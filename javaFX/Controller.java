65432ea9e1c294f13f6d5d46da813fc8534452e3
osama <osamaabukamel.12345@gmail.com>
2023-07-25 12:32:00 +0300

    Create Controller.java

---
+++ javaFX/Controller.java
@@ -0,0 +1,25 @@
+package javaFX;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+    // @FXML
+    // private Button addButton;
+    // @FXML
+    // private Button addCDButton;
+    // @FXML
+    // private Button removeButton;
+    // @FXML
+    // private Button searchButton;
+    // @FXML
+    // private Button updateButton;
+    // @FXML
+    // private Button libraryReportButton;
+    // @FXML
+    // private Button closeButton;
+
+    @FXML
+    private void handleAddButton(ActionEvent event) {
+    }
+}
