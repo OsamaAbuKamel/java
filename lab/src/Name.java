6ad6caac924395d28647a2a54b82025abf745827
osama <osamaabukamel.12345@gmail.com>
2023-07-25 13:00:16 +0300

    Create Name.java

---
+++ lab/src/Name.java
@@ -0,0 +1,50 @@
+public class Name {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+    public Name() {
+    }
+
+    public Name(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Name(String firstName, String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return this.middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return " " + getFirstName() +
+                ", " + getMiddleName() +
+                ", " + getLastName();
+    }
+
+}
