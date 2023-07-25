b5eb2ef91ac113816145ac7a39bbbee633af1535
osama <osamaabukamel.12345@gmail.com>
2023-07-25 12:44:00 +0300

    Create Node.java

---
+++ LinkedList/Node.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+public class Node<T> {
+    public T value;
+    public Node next;
+
+    public Node(T value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+}
