562719f1da60414a2656efd694b756759f6777d7
osama <osamaabukamel.12345@gmail.com>
2023-07-25 12:34:00 +0300

    Create MyStack.java

---
+++ Array_List/MyStack.java
@@ -0,0 +1,43 @@
+package Array_List;
+
+import java.util.ArrayList;
+
+public class MyStack<E> extends ArrayList<E> {
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return super.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return super.size();
+    }
+
+   @Override
+   public boolean add(E e) {
+       // TODO Auto-generated method stub
+       return super.add(e);
+   }
+
+ @Override
+ public boolean remove(Object o) {
+     // TODO Auto-generated method stub
+     return super.remove(o);
+ }
+
+   @Override
+   public E get(int index) {
+       // TODO Auto-generated method stub
+       return super.get(index);
+   }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+}
