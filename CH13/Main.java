1049ccc40c3069aa3c61ea5273b77eaa59c1a9c5
osama <osamaabukamel.12345@gmail.com>
2023-07-25 12:33:01 +0300

    Create Main.java

---
+++ CH13/Main.java
@@ -0,0 +1,29 @@
+package CH13;
+
+// interface Animal {
+//     void makeSound();
+//   }
+  
+//   class Dog implements Animal {
+//     @Override
+//     public void makeSound() {
+//       System.out.println("Woof!");
+//     }
+//   }
+  
+//   class Cat implements Animal {
+//     @Override
+//     public void makeSound() {
+//       System.out.println("Meow!");
+//     }
+//   }
+  
+  public class Main {
+    public static void main(String[] args) {
+      // Animal animal = new Dog();
+      // animal.makeSound();
+  
+      // animal = new Cat();
+      // animal.makeSound();
+    }
+  }
