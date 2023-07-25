2a509d9795a81522d030f4fae1029f5f5d79374a
osama <osamaabukamel.12345@gmail.com>
2023-07-25 12:46:08 +0300

    Create NodeTree.java

---
+++ Tree/src/NodeTree.java
@@ -0,0 +1,12 @@
+public class NodeTree {
+    int value;
+    NodeTree left;
+    NodeTree right;
+
+    public NodeTree(int value, NodeTree left, NodeTree right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+}
