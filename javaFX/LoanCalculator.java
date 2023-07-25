d54127a078defbca890517e7f10ba785397edd99
osama <osamaabukamel.12345@gmail.com>
2023-07-25 12:31:34 +0300

    Create LoanCalculator.java

---
+++ javaFX/LoanCalculator.java
@@ -0,0 +1,68 @@
+package javaFX;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class LoanCalculator extends Application {
+    private TextField tfAnnualInterestRate = new TextField();
+    private TextField tfNumberOfYears = new TextField();
+    private TextField tfLoanAmount = new TextField();
+    private TextField tfMonthlyPayment = new TextField();
+    private TextField tfTotalPayment = new TextField();
+    private Button btCalculate = new Button("Calculate");
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+        gridPane.add(new Label("Annual Interest Rate:"), 0, 0);
+        gridPane.add(tfAnnualInterestRate, 1, 0);
+        gridPane.add(new Label("Number of Years:"), 0, 1);
+        gridPane.add(tfNumberOfYears, 1, 1);
+        gridPane.add(new Label("Loan Amount:"), 0, 2);
+        gridPane.add(tfLoanAmount, 1, 2);
+        gridPane.add(btCalculate, 1, 3);
+
+        gridPane.add(new Label("Monthly Payment:"), 0, 4);
+        gridPane.add(tfMonthlyPayment, 1, 4);
+        gridPane.add(new Label("Total Payment:"), 0, 5);
+        gridPane.add(tfTotalPayment, 1, 5);
+        btCalculate.setOnAction(e -> CalculateLoanPayment());
+
+        gridPane.setAlignment(Pos.CENTER);
+        tfAnnualInterestRate.setAlignment(Pos.BOTTOM_RIGHT);
+        tfNumberOfYears.setAlignment(Pos.BOTTOM_RIGHT);
+        tfLoanAmount.setAlignment(Pos.BOTTOM_RIGHT);
+        tfMonthlyPayment.setAlignment(Pos.BOTTOM_RIGHT);
+        tfTotalPayment.setAlignment(Pos.BOTTOM_RIGHT);
+        tfMonthlyPayment.setEditable(false);
+        tfTotalPayment.setEditable(false);
+        GridPane.setHalignment(btCalculate, HPos.RIGHT);
+        Scene scene = new Scene(gridPane, 400, 250);
+        primaryStage.setTitle("LoanCalculator"); // Set title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show();
+
+    }
+
+    private void CalculateLoanPayment() {
+        double interest = Double.parseDouble(tfAnnualInterestRate.getText());
+        int year = Integer.parseInt(tfNumberOfYears.getText());
+        double loanAmount = Double.parseDouble(tfLoanAmount.getText());
+        Loan loan = new Loan(interest, year, loanAmount);
+        tfMonthlyPayment.setText(String.format("$%.2f", loan.getMonthlyPayment()));
+        tfTotalPayment.setText(String.format("$%.2f", loan.getTotalPayment()));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
