36e3950dd15cdd760d98e57260221e9061cb8200
osama <osamaabukamel.12345@gmail.com>
2023-07-25 13:00:26 +0300

    Create Address.java

---
+++ lab/src/Address.java
@@ -0,0 +1,48 @@
+
+public class Address {
+    private int streetAddress;
+    private String city;
+    private String state;
+
+    public Address() {
+    }
+
+    public Address(int streetAddress, String city, String state) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+    }
+
+    public int getStreetAddress() {
+        return this.streetAddress;
+    }
+
+    public void setStreetAddress(int streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return 
+            " STREET ADDRESS: " + getStreetAddress() + 
+        ", CITY: " + getCity() + 
+    ", STATE: " + getState() ;
+    }
+
+}
