a74292d7f136265e7cf16a51bd47b1d4068453d4
osama <osamaabukamel.12345@gmail.com>
2023-07-25 12:46:18 +0300

    Create DynamicArray.java

---
+++ ArrayDS/DynamicArray.java
@@ -0,0 +1,37 @@
+package ArrayDS;
+
+import java.util.Arrays;
+
+public class DynamicArray<T> {
+    Object[] data;
+    int size;
+
+    public DynamicArray() {
+        size = 0;
+        data = new Object[1];
+    }
+
+    public int getSize() {
+        return data.length;
+    }
+
+    public T get(int index) {
+        return (T) data[index];
+    }
+
+    public void put(int element) {
+        ensureCapacity(size + 1);
+        data[size++] = element;
+
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        int oldCapacity = data.length;
+        if (minCapacity > oldCapacity) {
+            int newCapacity = oldCapacity * 2;
+            if (newCapacity < minCapacity)
+                newCapacity = minCapacity;
+            data = Arrays.copyOf(data, newCapacity);
+        }
+    }
+}
